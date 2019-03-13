ddfdcc4e4955ab5eb293747c357bc82ad5c9da6e
matthewyu <matthewyu@hk01.com>
2019-03-13 23:44:06 +0800

    commit

---
+++ App/Containers/AuthForm/index.js
@@ -0,0 +1,81 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import { Platform, TouchableOpacity, View, StyleSheet } from 'react-native';
+import { AuthForm } from 'react-native-firebase-component'
+
+import userActions from '../../Redux/User/actions'
+import { connect } from 'react-redux';
+import firebase from 'react-native-firebase'
+import { setUserInfo } from '../../Helpers/firebase/DatabaseHelper'
+
+
+type Props = {};
+class AuthFormContainer extends Component<Props> {
+  static navigationOptions = {
+    header: null,
+  };
+
+  componentDidMount() {
+    const { navigation } = this.props
+    const { currentUser } = firebase.auth()
+    if (currentUser) {
+      navigation.navigate('Home')
+    }
+  }
+
+  render() {
+    const formAttributes = [
+      // { 
+      //   key: 'email',
+      //   label: 'Email',
+      //   valueType: 'text',
+      // },
+      // { 
+      //   key: 'password',
+      //   label: 'Password',
+      //   valueType: 'text',
+      //   secureTextEntry: true
+      // },
+    ]
+
+    const { navigation, completeLogin } = this.props
+
+    return (
+      <View style={{flex: 1, justifyContent: 'center', padding: 16}}>
+        <AuthForm 
+          formValues={{}}
+          formAttributes={formAttributes}
+          enableFacebookLogin={true}
+          onFacebookLoginSucess={(response) => {
+            const {
+              uid, 
+              displayName, 
+              email,
+              photoURL, 
+              phoneNumber,
+              refreshToken
+            } = response
+            const userInfo = {
+              uid,
+              displayName,
+              email,
+              photoURL,
+              phoneNumber,
+            }
+            setUserInfo(uid, userInfo)
+            navigation.navigate('Home')
+          }} />
+      </View>
+    );
+  }
+}
+
+
+export default  AuthFormContainer = connect(null)(AuthFormContainer);
--- App/Helpers/firebase/DatabaseHelper.js
+++ App/Helpers/firebase/DatabaseHelper.js
@@ -1,9 +1,18 @@
 import firebase from 'react-native-firebase'
-
+import moment from 'moment'
 
 // User
 export default {
   saveOctopusRecord: (userId, record) => {
-    firebase.database().ref(`User/${userId}/savingHistory`)
+    const newSavingHistoryRef = firebase.database().ref(`User/${userId}/savingHistory`).push()
+    newSavingHistoryRef.set({
+      ...record,
+      id: newSavingHistoryRef.key,
+      createdTs: moment().unix().utc()
+    })
+  },
+  setUserInfo: (userId, userInfo) => {
+    const userInfoRef = firebase.database().ref(`User/${userId}/userInfo`)
+    userInfoRef.set(userInfo)
   }
 }
